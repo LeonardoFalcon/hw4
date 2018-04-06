a9d076eceee22672010364196ce7ea5cd3ac3bd1
LeonardoFalcon <leonardo.falconizzi@geappliances.com>
2018-04-05 20:11:29 -0400

    average the chunks of data to generate test/training data

--- DoorEventCapture.py
+++ DoorEventCapture.py
@@ -9,7 +9,7 @@ csvfile = "/Users/leonardofalcon/Documents/NCSU/ECE792/Homework 4/doorEvent.csv"
 
 #design parameters
 bufferSize = 100 #samples taken for the start-up average calculation
-standarDeviationsToStart = 3 #number of standard deviations that indicate door event is starting
+standarDeviationsToStart = 4 #number of standard deviations that indicate door event is starting
 numberOfChunks = 5 #number of chunks we want data to be broken up into
 numberOfSamplesAboveThresholdToStart = 5
 numberOfSamplesBelowThresholdToStop = 50
@@ -25,11 +25,28 @@ counter = 0
 startDataCaptureCount = 0
 stopDataCaptureCount = 0
 
+testData = []
+
+def mean(data):
+    """Return the sample arithmetic mean of data."""
+    n = len(data)
+    if n < 1:
+        raise ValueError('mean requires at least one data point')
+    return sum(data)/float(n) # in Python 2 use sum(data)/float(n)
+
 def chunks(lst, div):
     lst = [ lst[i:i + len(lst)/div] for i in range(0, len(lst), len(lst)/div) ] #Subdivide list.
     if len(lst) > div: # If it is an uneven list.
         lst[div-1].extend(sum(lst[div:],[])) # Take the last part of the list and append it to the last equal division.
     return lst[:div] #Return the list up to that point.
+
+def generateTestData(lst):
+    global testData
+
+    for i in lst:
+        testData.append(mean(i))
+    print("testData:")
+    print(testData)
 
 def on_connect(client, userdata, flags, rc):
    print("Connected with result code "+str(rc))
@@ -60,24 +77,17 @@ def on_message(client, userdata, msg):
                    if eventActive == 1:
                        print("stop")
                        dataInChunks = list(chunks(list(doorEventData), numberOfChunks)) #this will be the data chunks
+                       generateTestData(dataInChunks)
                        #Assuming res is a flat list
                        with open(csvfile, "w") as output:
                            print("writing to file")
                            writer = csv.writer(output, lineterminator='\n')
                            for val in list(doorEventData):
                                writer.writerow([val])
-                       print(dataInChunks)
                    doorEventData.clear()
                    startDataCaptureCount = 0
                    stopDataCaptureCount = 0
                    eventActive = 0
-
-def mean(data):
-    """Return the sample arithmetic mean of data."""
-    n = len(data)
-    if n < 1:
-        raise ValueError('mean requires at least one data point')
-    return sum(data)/float(n) # in Python 2 use sum(data)/float(n)
 
 def _ss(data):
     """Return sum of square deviations of sequence data."""
