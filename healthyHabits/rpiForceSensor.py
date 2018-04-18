e3d3cdfb001c6d0865e03f630e1ed0253ba1433c
LeonardoFalcon <leonardo.falconizzi@geappliances.com>
2018-04-17 20:44:55 -0400

    add mqtt

--- healthyHabits/rpiForceSensor.py
+++ healthyHabits/rpiForceSensor.py
@@ -2,13 +2,19 @@ import serial
 import time
 import datetime
 from time import strftime, sleep
+import paho.mqtt.client as mqtt
 
+brokerIp = "192.168.86.77" #LAN
+sleepTopic = "sleepFeature"
+currentlySleepingTopic = "sleepActive"
 filterReady = False
 avg = 0
 sum = 0
 sampleCount = 0
 onBedThreshold = 700
 onTheBedFlag = False
+maxSleep = 720 #in minutes - 12 hours
+minSleep = 240 #in minutes - 4 hours
 
 def feed(val):
    global filterReady
@@ -29,25 +35,25 @@ def feed(val):
 
 def stopWatch(value):
     '''From seconds to Days;Hours:Minutes;Seconds'''
+    print(value)
 
-    valueD = (((value/365)/24)/60)
-    Days = int (valueD)
+def on_connect(client, userdata, flags, rc):
+   print("Connected with result code "+str(rc))
 
-    valueH = (valueD-Days)*365
-    Hours = int(valueH)
+def on_message(client, userdata, msg):
+   print("msg received")
 
-    valueM = (valueH - Hours)*24
-    Minutes = int(valueM)
-
-    valueS = (valueM - Minutes)*60
-    Seconds = int(valueS)
-
-
-    print Days,";",Hours,":",Minutes,";",Seconds
-
-ser=serial.Serial("/dev/ttyACM0",9600)  #change ACM number as found from ls /dev/tty/ACM*
+ser=serial.Serial("/dev/ttyACM0",9600)  #setup communication to Arduino
 ser.baudrate=9600
 force = 0
+
+client = mqtt.Client(client_id="5", clean_session=False)
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(brokerIp, 1883, 60)
+
+client.loop_start()
 
 with open("cpu_temp.csv", "a") as log:
    while True:
@@ -67,11 +73,13 @@ with open("cpu_temp.csv", "a") as log:
          if avg > onBedThreshold:
             if onTheBedFlag == False:
                onTheBedFlag = True
+               client.publish(currentlySleepingTopic, payload=1, qos=2, retain=False)
                start = time.time()
                print("on the bed")
          else:
             if onTheBedFlag == True:
                onTheBedFlag = False
+               client.publish(currentlySleepingTopic, payload=0, qos=2, retain=False)
                end = time.time()
                stopWatch(end-start)
                print("off the bed")
